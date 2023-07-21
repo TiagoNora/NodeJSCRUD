a3d0db39025624dd39be4690c99e6937cada0f95
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-07-21 03:20:46 +0100

    Adding routes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,22 @@
-const getAll = (req, res, next) => {
+const getAllProducts = (req, res) => {
     res.json({message: "Get all products"});
 };
 
-module.exports = {getAll};
+const showProduct = (req, res) => {
+    res.json({message: "Get a product"});
+};
+
+const createProduct = (req, res) => {
+    res.json({message: "Create a product"});
+};
+
+const updateProduct = (req, res) => {
+    res.json({message: "Update a product"});
+};
+
+const deleteProduct = (req, res) => {
+    res.json({message: "Delete a product"});
+};
+
+
+module.exports = {getAllProducts,showProduct,createProduct,updateProduct,deleteProduct}
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get("/products", productController.getAll);
+router.get("/products", productController.getAllProducts);
+router.get("/product", productController.showProduct);
+router.post("/product", productController.createProduct);
+router.delete("/product", productController.deleteProduct);
+router.put("/product", productController.updateProduct);
 
 module.exports = router;
